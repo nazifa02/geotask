281466a8aba13bc96d43b014e5388c4703466c13
Syeda Nazifa Tasneem <nazifadu.g57@gmail.com>
2024-05-06 10:12:01 -0400

    Bump version: 0.0.5 → 0.0.6

--- geotask/__init__.py
+++ geotask/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Syeda Nazifa Tasneem"""
 __email__ = "nazifadu.g57@gmail.com"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 from .geotask import Map
 #from .temp import save_yearly_images
