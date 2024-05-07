a74bed77ec76ed4b81d21141aa1c47fef26844cb
Syeda Nazifa Tasneem <nazifadu.g57@gmail.com>
2024-05-06 23:07:24 -0400

    update6

--- geotask/__init__.py
+++ geotask/__init__.py
@@ -5,4 +5,5 @@ __email__ = "nazifadu.g57@gmail.com"
 __version__ = "0.0.6"
 
 from .geotask import Map
-#from .temp import save_yearly_images
+#from .temp import download_file, save_yearly_images
+from geotask.temp import download_file, save_yearly_images
